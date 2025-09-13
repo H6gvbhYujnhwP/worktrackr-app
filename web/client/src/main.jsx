ea62a07f202a5a8cb4af13336fdbfd92e26259f0
H6gvbhYujnhwP <admin@thegreenagents.com>
2025-09-13 09:04:27 +0100

    Update main.jsx

--- web/client/src/main.jsx
+++ web/client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Pricing from './Pricing.jsx' // add this file if you haven't already (or keep pricing inside App)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pricing" element={<Pricing />} />
+        {/* add more routes as you build them */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
