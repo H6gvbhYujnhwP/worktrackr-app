aa6a97707487dd608015a3d29f677e024f4c4ff4
H6gvbhYujnhwP <admin@thegreenagents.com>
2025-09-24 17:03:16 +0100

    Update main.jsx

--- web/client/src/main.jsx
+++ web/client/src/main.jsx
@@ -1,19 +1,14 @@
+// web/client/src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
-import Pricing from './Pricing.jsx'     // ensure case matches your file
-import SignUp from './SignUp.jsx'       // new page
+import RootApp from './App.jsx' // App.jsx exports the router (RootApp)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/pricing" element={<Pricing />} />
-      </Routes>
+      <RootApp />
     </BrowserRouter>
   </StrictMode>
 )
